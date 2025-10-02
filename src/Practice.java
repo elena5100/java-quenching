c11efb09549641778ddf4b780e17ae89732bf9cf
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-10-01 23:32:01 -0700

    return number of levels in tree(levelCount) & return sum of nodes at given level(sumAtLevel)

--- src/Practice.java
+++ src/Practice.java
@@ -139,7 +139,12 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+            if (root == null) return 0;
+        
+            int left = levelCount(root.left);
+            int right = levelCount(root.right);
+        
+            return 1 + Math.max(left, right);
     }
 
 
@@ -167,7 +172,13 @@ public class Practice {
      * @return the sum of the nodes at the given level
      */
     public static int sumAtLevel(BinaryTreeNode<Integer> root, int level) {
-        return 0;
+        if(root == null) return 0; 
+        // at this level, return value
+        if(level == 1) return root.data;
+
+        int sum = sumAtLevel(root.left, level -1) + sumAtLevel(root.right, level -1);
+
+        return sum;
     }
 
 
